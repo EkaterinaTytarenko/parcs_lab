f6254cfd8f7a3d61a6654c9b82d89e722826f21e
Katya <katerina.tytarenko@gmail.com>
2021-03-03 21:12:39 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import parcs.*;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 
@@ -9,22 +12,33 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         task curtask = new task();
-        curtask.addJarFile("Abbreviations.jar");
+        curtask.addJarFile("Vernam.jar");
         AMInfo info = new AMInfo(curtask, null);
+
+        FileReader fileReader = new FileReader("input.txt");
+        String line;
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
         long startTime = System.nanoTime();
 
-        point p1 = info.createPoint();
-        channel c1 = p1.createChannel();
-        p1.execute("Abbreviations");
-        c1.write("ghsvhc GGHH fghjkfgh ING VFGH fghjklkjhgffgh");
-        List<String> res= (List<String>)c1.readObject();
-        for(String str: res)
-            System.out.println(str);
+        while ((line = bufferedReader.readLine()) != null) {
+            point p = info.createPoint();
+            channel c = p.createChannel();
+
+            p.execute("Vernam");
+
+            c.write(line);
+            String result=(String) c.readObject();
+
+            System.out.println("Original sentence:");
+            System.out.println(line);
+            System.out.println("Cipher text:");
+            System.out.println(result);
+        }
 
         double estimatedTime = (double) (System.nanoTime() - startTime) / 1000000000;
         System.out.println("Total time: " + estimatedTime);
 
-        System.out.println("Successfully converted colored images into negative images");
         curtask.end();
     }
 }
---
+++ src/Vernam.java
@@ -0,0 +1,59 @@
+import java.io.Serializable;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import parcs.*;
+
+public class Vernam implements AM {
+
+    private final static char encryptionArr[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+    public void run(AMInfo info){
+
+        String text = (String) info.parent.readObject();
+
+        String key=generateKey(text.length());
+        String encryptedString=encrypt(text,key);
+
+        info.parent.write((Serializable) encryptedString);
+    }
+
+    public String encrypt(String plainText, String key){
+        //Make sure the key and the plainText are the same length.
+        if(plainText.length() != key.length()){
+            return null;
+        }
+
+        byte[] plainTextBytes = plainText.getBytes();
+        byte[] keyBytes = key.getBytes();
+        byte[] encryptedText = new byte[plainTextBytes.length];
+        for(int i = 0; i < plainTextBytes.length; i++){
+            encryptedText[i] = (byte) (keyBytes[i] ^ plainTextBytes[i]);
+        }
+
+        return new String(encryptedText);
+    }
+
+    public String generateKey(int length){
+        if(length <= 0){
+            return null;
+        }
+        String key = "";
+        SecureRandom secureRandom = new SecureRandom();
+        try {
+            secureRandom = SecureRandom.getInstance("SECURERAND");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        //Builds the key.
+        for(int i = 0; i < length; i++){
+            int randomValue = secureRandom.nextInt(26);
+            key += encryptionArr[randomValue];
+        }
+        return key;
+    }
+
+}
