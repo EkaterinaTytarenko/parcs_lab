70a88d1f7cddfe900160698619f15a00b4a4032c
Katya <katerina.tytarenko@gmail.com>
2021-03-03 16:04:52 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         task curtask = new task();
+        System.out.println("1");
         curtask.addJarFile("Negative.jar");
         AMInfo info = new AMInfo(curtask, null);
 
         long startTime = System.nanoTime();
-
+        System.out.println("2");
         File dir = new File("images");
         File[] directoryListing = dir.listFiles();
         if (directoryListing != null) {
@@ -23,17 +24,21 @@ public class Main {
                 File img=directoryListing[i];
                 point p = info.createPoint();
                 channel c = p.createChannel();
+                System.out.println("4");
                 p.execute("Negative");
                 c.write(img);
                 BufferedImage result=(BufferedImage) c.readObject();
                 File file = new File("images/negative"+i+".jpg");
+                System.out.println(result);
+                System.out.println(file);
                 ImageIO.write( result, "jpg", file);
+                System.out.println("5");
 
             }
         }
-
+        System.out.println("6");
         double estimatedTime = (double) (System.nanoTime() - startTime) / 1000000000;
-        System.out.println("Time total (excluding IO): " + estimatedTime);
+        System.out.println("Total time: " + estimatedTime);
 
         System.out.println("Successfully converted colored images into negative images");
         curtask.end();
