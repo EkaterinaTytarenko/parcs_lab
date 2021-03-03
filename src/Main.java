bcaca1fb3331531ce9c00a988629f36bcc43f395
Katya <katerina.tytarenko@gmail.com>
2021-03-03 16:45:51 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -11,20 +11,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         task curtask = new task();
-        System.out.println("1");
         curtask.addJarFile("Negative.jar");
         AMInfo info = new AMInfo(curtask, null);
-
         long startTime = System.nanoTime();
-        System.out.println("2");
+
         File dir = new File("images");
         File[] directoryListing = dir.listFiles();
+
         if (directoryListing != null) {
             for (int i=0;i<directoryListing.length;++i) {
                 File img=directoryListing[i];
                 point p = info.createPoint();
                 channel c = p.createChannel();
-                System.out.println("4");
                 p.execute("Negative");
                 c.write(img);
                 BufferedImage result=(BufferedImage) c.readObject();
@@ -32,11 +30,10 @@ public class Main {
                 System.out.println(result);
                 System.out.println(file);
                 ImageIO.write( result, "jpg", file);
-                System.out.println("5");
 
             }
         }
-        System.out.println("6");
+
         double estimatedTime = (double) (System.nanoTime() - startTime) / 1000000000;
         System.out.println("Total time: " + estimatedTime);
 
