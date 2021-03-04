d92126be4f30a400034bf445b9f5e44a1f286dac
EkaterinaTytarenko <48032581+EkaterinaTytarenko@users.noreply.github.com>
2021-03-04 14:37:06 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -25,19 +25,48 @@ public class Main {
 
         long startTime = System.nanoTime();
 
-        point p = info.createPoint();
-        channel c = p.createChannel();
-       
-        p.execute("Vernam");
-        c.write(text1+text2+text3+text4);
+        point p1 = info.createPoint();
+        channel c1 = p1.createChannel();
+        point p2 = info.createPoint();
+        channel c2 = p2.createChannel();
+        point p3 = info.createPoint();
+        channel c3 = p3.createChannel();
+        point p4 = info.createPoint();
+        channel c4 = p4.createChannel();
 
-        String result=(String)c.readObject();
+        p1.execute("Vernam");
+        c1.write(text1);
+        p2.execute("Vernam");
+        c2.write(text2);
+        p3.execute("Vernam");
+        c3.write(text3);
+        p4.execute("Vernam");
+        c4.write(text4);
+
+        String result1=(String)c1.readObject();
         System.out.println("Original sentence:");
-        System.out.println(text1+text2+text3+text4);
+        System.out.println(text1);
         System.out.println("Cipher text:");
-        System.out.println(result);
+        System.out.println(result1);
 
- 
+        String result2=(String)c2.readObject();
+        System.out.println("Original sentence:");
+        System.out.println(text2);
+        System.out.println("Cipher text:");
+        System.out.println(result2);
+
+        String result3=(String)c3.readObject();
+        System.out.println("Original sentence:");
+        System.out.println(text3);
+        System.out.println("Cipher text:");
+        System.out.println(result3);
+
+        String result4=(String)c4.readObject();
+        System.out.println("Original sentence:");
+        System.out.println(text4);
+        System.out.println("Cipher text:");
+        System.out.println(result4);
+
         double estimatedTime = (double) (System.nanoTime() - startTime) / 1000000000;
         System.out.println("Total time: " + estimatedTime);
 
